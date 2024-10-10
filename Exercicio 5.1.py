088da9e969fbb32175460a1d8cd2e4051cebc6de
ThomasLima9 <165452976+ThomasLima9@users.noreply.github.com>
2024-10-10 20:52:54 -0300

    Update Exercicio 5.1.py

--- Exercicio 5.1.py
+++ Exercicio 5.1.py
@@ -1,16 +1,12 @@
-m = 0  # Inicializa a variável 'm' com 0. 'm' armazenará o maior número inserido pelo usuário.
+lista = []
 
-# Solicita ao usuário que insira um número
-numero = int(input('Digite um numero: '))
+while True:
+    n = input('Digite um numero: ')
+    if n != 'end':
+        n = int(n)
+        lista.append(n)
 
-# Enquanto o número inserido for maior ou igual a 0, o loop continuará
-while numero >= 0:
-    # Se o número inserido for maior que o valor armazenado em 'm', atualiza 'm' com o número
-    if numero > m:
-        m = numero  # 'm' recebe o novo maior valor
-    
-    # Solicita outro número ao usuário para continuar o loop
-    numero = int(input('Digite um numero: '))
+    else:
+        break
 
-# Quando um número negativo for inserido, o loop termina e o maior número inserido é exibido
-print(f"Maior numero é {m}")
+print(f'O maior valor é ', max(lista) )
