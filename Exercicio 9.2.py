2d7e7f1ea30565c7f781db084b0516e09cd0e988
ThomasLima9 <165452976+ThomasLima9@users.noreply.github.com>
2024-10-10 21:29:56 -0300

    Create Exercicio 9.2.py

---
+++ Exercicio 9.2.py
@@ -0,0 +1,30 @@
+
+ListaA = []
+for a in range(1, 11):
+    ListaA.append(a)
+print("Lista A:", ListaA)
+
+ListaB = []
+for b in range(10, 0, -1): 
+    ListaB.append(b)
+print("Lista B:", ListaB)
+
+ListaC = []
+for c in range(10, 31, 5):  
+    ListaC.append(c)
+print("Lista C:", ListaC)
+
+ListaD = []
+for d in range(-20, 21, 8): 
+    ListaD.append(d)
+print("Lista D:", ListaD)
+
+ListaE = []
+for e in range(10, 5, -2): 
+    ListaE.append(e)
+print("Lista E:", ListaE)
+
+ListaF = []
+for f in range(15, -1, -3):  
+    ListaF.append(f)
+print("Lista F:", ListaF)
